cb54dfeecdac68fb364c781e074ac8196c19be7c
Ryman White <rymanwhite@gmail.com>
2019-12-17 07:14:31 +1100

    Progress on all the looping!

--- src/components/Plan.vue
+++ src/components/Plan.vue
@@ -5,41 +5,40 @@
         <div v-for="(value, name, index) in meal_plan" v-bind:key="index">
             <h2>{{ name }}</h2>
             <h3>Lunch</h3>
-            <select v-model="value.lunch.recipe">
+            <select v-model="value.lunch.recipe" v-on:change="mealPlanChange">
                 <option value="" selected></option>
                 <option v-for="(item, index) in menu" v-bind:key="index">
                     {{ item }}
                 </option>
             </select>
             <p v-if="value.lunch.recipe != ''">
-            <button @click="value.lunch.serves -= 1">-</button>
+            <button @click="value.lunch.serves -= 1" v-on:click="mealPlanChange">-</button>
             {{ value.lunch.serves }}
-            <button @click="value.lunch.serves += 1">+</button>
+            <button @click="value.lunch.serves += 1" v-on:click="mealPlanChange">+</button>
             </p>
             <h3>Dinner</h3>
-            <select v-model="value.dinner.recipe">
+            <select v-model="value.dinner.recipe" v-on:change="mealPlanChange">
                 <option value="" selected></option>
                 <option v-for="(item, index) in menu" v-bind:key="index">
                     {{ item }}
                 </option>
             </select>
             <p v-if="value.dinner.recipe != ''">
-            <button @click="value.dinner.serves -= 1">-</button>
+            <button @click="value.dinner.serves -= 1" v-on:click="mealPlanChange">-</button>
             {{ value.dinner.serves }}
-            <button @click="value.dinner.serves += 1">+</button>
+            <button @click="value.dinner.serves += 1" v-on:click="mealPlanChange">+</button>
             </p>
         </div>
     </div>
 </template>
 
 <script>
-/* import Recipes from '../assets/Recipes.json'; */
 
 import { eventBus } from '../event-bus';
 
 export default {
   data () {
-      console.log("start of data");
+      //console.log("start of data");
 
    return {
       menu: [],
@@ -136,7 +135,7 @@ export default {
   },
   watch: { 
         'meal_plan': { 
-            immediate: false,
+            //immediate: false,
             handler: function() {
                 console.log("watch kicked in!");
                 for (var meal in this.meal_plan) {
@@ -147,62 +146,51 @@ export default {
                         this.meal_plan[meal].dinner.serves = 0;
                     }
                 }
-                //this.meal_plan_summary_builder();
-/*                 eventBus.$emit('meal_plan_edit', this.meal_plan); 
-                console.log("huh?")
-                console.log(this.meal_plan) */
-                //this.meal_plan_summary = 
-
-            //NEED TO ADD IN SOMETHING CONDITIONAL HERE TO CONTROL WHEN THESE EVENTS FIRE!!    
-                this.meal_plan_summary_builder()
-                //console.log("is the meal plan summary a thing??")
-                console.log(this.meal_plan_summary)
-                eventBus.$emit('meal_plan_summary_change', this.meal_plan_summary);
-                localStorage.setItem('meal_plan', JSON.stringify(this.meal_plan)) 
+            localStorage.setItem('meal_plan', JSON.stringify(this.meal_plan))
             },
             deep: true,
             },
   },
 
+  computed: {
+
+  },
+
   methods: {
-      meal_plan_summary_builder: function(){
-           let component = this;
-           let meals = [];
-           //console.log("hey");
-           let new_meal_plan = component.meal_plan;
-           //console.log(new_meal_plan);
-           let keys = Object.keys(new_meal_plan)
-           //console.log(keys);
-           let times = ['lunch','dinner'];
-           //console.log(keys)
-           for (var key in keys){
-               for (var time in times){                   
-                   if (new_meal_plan[keys[key]][times[time]].recipe != ''){
-                       let meal = new_meal_plan[keys[key]][times[time]].recipe;
-                       let serves = new_meal_plan[keys[key]][times[time]].serves;
-                       //console.log(meal);
-                       //console.log(meals.findIndex(x => x.recipe == meal));
-                       if (meals.findIndex(x => x.recipe == meal) == -1){
-                           console.log("There's no " + meal + " already in the meal plan")
-                           //add a push to the meals array here
-                           meals.push({
-                               recipe: meal,
-                               serves: serves
-                           })
-                       } else {
-                           console.log(meal + " is already in the list. Just updating the serve")
-                           //update the serve for the meal in the meals array
-                           let index = meals.findIndex(x => x.recipe == meal);
-                           //console.log(meals[index].serves);
-                           meals[index].serves = meals[index].serves + serves;
-                           //console.log(meals[index].serves)
-                       }
-                       }
-               }
-           } 
-           //console.log("component");
-           component.meal_plan_summary = meals;
-           console.log(component.meal_plan_summary);       
+            mealPlanChange: function(){
+                console.log("mealPlanChange")
+                this.meal_plan_summary_builder()
+                console.log(this.meal_plan_summary)
+                eventBus.$emit('meal_plan_summary_change', this.meal_plan_summary);
+                localStorage.setItem('meal_plan', JSON.stringify(this.meal_plan)) 
+            },
+            meal_plan_summary_builder: function(){
+                let component = this;
+                let meals = [];
+                let new_meal_plan = component.meal_plan;
+                let keys = Object.keys(new_meal_plan)
+                let times = ['lunch','dinner'];
+                for (var key in keys){
+                    for (var time in times){                   
+                        if (new_meal_plan[keys[key]][times[time]].recipe != ''){
+                            let meal = new_meal_plan[keys[key]][times[time]].recipe;
+                            let serves = new_meal_plan[keys[key]][times[time]].serves;
+                            if (meals.findIndex(x => x.recipe == meal) == -1){
+                                console.log("There's no " + meal + " already in the meal plan")
+                                meals.push({
+                                    recipe: meal,
+                                    serves: serves
+                                })
+                            } else {
+                                console.log(meal + " is already in the list. Just updating the serve")
+                                let index = meals.findIndex(x => x.recipe == meal);
+                                meals[index].serves = meals[index].serves + serves;
+                            }
+                            }
+                    }
+                } 
+           // Need to confirm if I'm doubling up things here assignging to the component and then returning     
+           component.meal_plan_summary = meals;      
            return component.meal_plan_summary  
        },
       reset_planner(){
--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -42,8 +42,6 @@ import { eventBus } from '../event-bus';
 export default {
     data ()  {
         return {
-            //meal_plan: {},
-            //meal_plan_summary: Array,
             shopping_list: [],
             custom_list: []
         }
@@ -75,84 +73,51 @@ export default {
             console.log(index)
             this.shopping_list[index].removed = true;
             this.shopping_list[index].amount = 0;
-            //Do I actually want to remove this item, or just declare that it has been removed. 
-            //The item has to be able to return on a meal plan edit. I think it's easier just to remove it
-            //And then scrap the 'removed' true/false from the item.
-            /* let list = this.shopping_list;
-            list.splice(index,1); */
-
+        //TO DO - make the list able to parse a 'removed' item
         },
 
         edit_list: function(){
-        
-        let delete_buttons = document.getElementsByClassName('delete-button')
-        console.log(delete_buttons)
-
-      /*   for (let elem in delete_buttons){
-            delete_buttons[elem].classList.toggle("display-toggle");
-        } */
-       
-        for (let i = 0; i < delete_buttons.length; i++) {
-            if (delete_buttons.item(i).style.display == "inline"){
-                
-                delete_buttons.item(i).style.display = "none";
-                //document.getElementById("edit_list_button").src = "icons/edit.png";
-            } else {
-                delete_buttons.item(i).style.display = "inline";
-                //document.getElementById("edit_list_button").src = "icons/check.png";
-            }
-        } 
+            //TO DO - add in icons for delete buttons
+            let delete_buttons = document.getElementsByClassName('delete-button')
+            for (let i = 0; i < delete_buttons.length; i++) {
+                if (delete_buttons.item(i).style.display == "inline"){                
+                    delete_buttons.item(i).style.display = "none";
+                    //document.getElementById("edit_list_button").src = "icons/edit.png";
+                } else {
+                    delete_buttons.item(i).style.display = "inline";
+                    //document.getElementById("edit_list_button").src = "icons/check.png";
+                }
+            } 
     },
     check_click_custom: function(item){
-           console.log("hey custom " + item);
            let index = this.custom_list.findIndex(x => x.item == item);
-           console.log(this.custom_list)
-           console.log("inded?" +index);
            if(this.custom_list[index].checked == true){
                this.custom_list[index].checked = false
            } else {
                this.custom_list[index].checked = true;
            }
-
-           
-           /* let index = this.shopping_list.findIndex(x => x.item == item);
-           this.shopping_list[index].checked = true; */
        },
-       check_click: function(item){
-           console.log("hey " + item);
-           let index = this.shopping_list.findIndex(x => x.item == item);
-           if (this.shopping_list[index].checked == true){
-               this.shopping_list[index].checked = false;
-           } else {
-               this.shopping_list[index].checked = true;
-           }
-        
+    check_click: function(item){
+            let index = this.shopping_list.findIndex(x => x.item == item);
+            if (this.shopping_list[index].checked == true){
+                this.shopping_list[index].checked = false;
+            } else {
+                this.shopping_list[index].checked = true;
+            }
+            
        },
-/*        shop_list_difference_checker: function(long_list, short_list){
+    shopping_list_builder: function(plan_summary){
+            let list = []
+            console.log("In the shopping list builder");
+            for (var meal in plan_summary){
+                let recipe = plan_summary[meal].recipe;
+                let serves = plan_summary[meal].serves;
+                let index = Recipes.findIndex(x => x.recipe == recipe);
+                let ingredients = Recipes[index].ingredients;
 
-           for (let item in long_list
-       }, */
-       shop_list_builder_2: function(plan_summary){
-           let list = []
-           console.log("inside the shopping list builder");
-           //console.log(plan_summary);
-           for (var meal in plan_summary){
-               let recipe = plan_summary[meal].recipe;
-               let serves = plan_summary[meal].serves;
-               //console.log(recipe + " & " + serves)
-
-               let index = Recipes.findIndex(x => x.recipe == recipe);
-               //console.log(index);
-
-               let ingredients = Recipes[index].ingredients;
             for (var item in ingredients){
                 let measure = (ingredients[item].amount/Recipes[index].serves)*serves;
-                //console.log(measure);
-
                 let list_index = list.findIndex(x => x.item == ingredients[item].name)
-                /* console.log("the index is:")
-                console.log(list_index);
-                console.log(ingredients[item].name); */
 
                 if (list_index == -1){
                     let list_item = {
@@ -162,169 +127,75 @@ export default {
                                 checked: false,
                                 removed: false
                             }
-                    //console.log(list_item);
                     list.push(list_item)
                 } else {
                     list[list_index].amount = list[list_index].amount + measure;
+                    }
                 }
-
-         /*        if (meals.findIndex(indexOf) == -1){
-                        meals.push(meal)
-                    } else {
-                        let i = meals.findIndex(indexOf)
-                        let key = Object.keys(meals[i])
-                        meals[i][key] += Number(s)
-                    } */
-
-
-                
-                }
-               
-
-           }
-           console.log("the new list!")
-           return list;
-       }
+            }
+            return list;
+        }
     },
     created () {
-        console.log("this is created!");
-
-        /* if (localStorage.meal_plan) {
-            this.meal_plan = JSON.parse(localStorage.getItem('meal_plan'));
-            } */
-        //let app = this;
         if (localStorage.shopping_list) {
           this.shopping_list = JSON.parse(localStorage.getItem('shopping_list'));
-          console.log("shopping list set from local storage")
-
+          console.log("Shopping list set from local storage")
           } else {
-              console.log("no local storage to build a list from");
+              console.log("No local storage to build a list from");
               localStorage.setItem('shopping_list', JSON.stringify(this.shopping_list))
-              
-          //this.shopping_list = this.shopping_list_builder(this.meal_plan);
          }  
 
-         if (localStorage.custom_list) {
-          this.custom_list = JSON.parse(localStorage.getItem('custom_list'));
-          console.log("custom list set from local storage")
-
-          } else {
-              console.log("no local storage to build a list from");
-              localStorage.setItem('custom_list', JSON.stringify(this.custom_list))
-              
-          //this.shopping_list = this.shopping_list_builder(this.meal_plan);
+        if (localStorage.custom_list) {
+        this.custom_list = JSON.parse(localStorage.getItem('custom_list'));
+        console.log("custom list set from local storage")
+        } else {
+            console.log("no local storage to build a list from");
+            localStorage.setItem('custom_list', JSON.stringify(this.custom_list))
          } 
-/*         eventBus.$on('meal_plan_edit', new_meal_plan => {
-            this.shopping_list = this.shopping_list_builder(new_meal_plan);
-                    });   */ 
-        /* eventBus.$on('meal_plan_edit', new_meal_plan => {
-            this.meal_plan_summary = this.meal_plan_summary_builder();
-            console.log(new_meal_plan);
-                    });  */
+
         eventBus.$on('meal_plan_summary_change', new_summary => {
             let component = this;
             let delete_buttons = document.getElementsByClassName('delete-button')
-            console.log(delete_buttons)
-
-            /*   for (let elem in delete_buttons){
-                    delete_buttons[elem].classList.toggle("display-toggle");
-                } */
-            
-                for (let i = 0; i < delete_buttons.length; i++) {
-                        
-                        delete_buttons.item(i).style.display = "none";
-                        //document.getElementById("edit_list_button").src = "icons/edit.png";
-                    } 
-
-            console.log("the new summary");
-            console.log(new_summary);
-            console.log("the test");
-            // The new summary passed in from the plan page is now going to be run through the shopping list builder
-            //ONCE THE SHOPPING LIST BUILDER 2 function is working, need to turn off the eventbus for the old shopping list builder
-                //and make sure that I can bring in a new plan and compare with the current BEFORE overriding.
-            let new_list = this.shop_list_builder_2(new_summary);
-            console.log(new_list);
-            let new_list_length = new_list.length
-            console.log("new list length")
-            console.log(new_list_length)
-            /* Use this to assign the correct meal_plan summary to the data for the page when needed
-            component.meal_plan_summary = new_summary;
-            console.log(component.meal_plan_summary); */
+            for (let i = 0; i < delete_buttons.length; i++) {
+                    delete_buttons.item(i).style.display = "none";
+                    //document.getElementById("edit_list_button").src = "icons/edit.png";
+                } 
 
             //Now checking if the new_list matches the current list
-            console.log("local storage shopping list");
-            console.log(localStorage.shopping_list)
+            let new_list = this.shopping_list_builder(new_summary);
+            let new_list_length = new_list.length
             let current_list = JSON.parse(localStorage.getItem('shopping_list'));
-            console.log("the current list");
-            console.log(current_list);
             let current_list_length = current_list.length
-            console.log("current list length")
-            console.log(current_list_length)
-            //Interesting that these lists are already returning different values... Weird caching going on!
-
-            //the new summary is being passed to the shopping list whenever a change is made to the meal plan!
-            //now this needs to check if there is currently a meal_plan_summary present in this component
-            //if there is, it needs to check itself against it 
-            //Only then may you delete the V2 code from above and bring in a single shopping list that is built 
-            // Ooooor, maybe in this event, I call a function to build a shopping list, then grab the current shopping 
-                //list and check that with the new one.  – yep, I think that will work.
 
 
             //if current list is empty, then set it to the new list
             if (current_list_length === 0){
                 console.log("the original list is empty, so you can just assign the new list to being the current list!");
                 component.shopping_list = new_list;
-                console.log("component shopping list:");
-                console.log(component.shopping_list);
-                localStorage.setItem('shopping_list', JSON.stringify(component.shopping_list))
-                console.log("local storage shopping list");
-                console.log(localStorage.shopping_list)
+                localStorage.setItem('shopping_list', JSON.stringify(component.shopping_list));
             } else if (current_list_length > new_list_length){
                 let placeholder_list = current_list.slice()
-                console.log("placeholder list");
-                console.log(placeholder_list);
                  // if current list is longer than the new list, need to remove some items before returning the list
                 console.log("the current list is longer, so we'll need to remove some items!");
-                console.log(current_list);
-                //let holder_list = current_list;
-                /* let holder_list = current_list.slice();
-                console.log("holder list");
-                console.log(holder_list); */
                 for (let list_item in current_list){
-                    console.log("item! be a number")
-                    console.log(list_item)
                     let index = new_list.findIndex(x => x.item == current_list[list_item].item);
-                    console.log("the index of the item")
-                    console.log(index)
                     if (index === -1){
-                        console.log("this item needs to be removed. Index:");
                         let placeholder_index = placeholder_list.findIndex(x => x.item == current_list[list_item].item)
                         placeholder_list.splice(placeholder_index,1);
-                        console.log(placeholder_list);
-                        /* console.log(current_list[list_item].item);
-                        current_list.splice(list_item,1);
-                        console.log(JSON.stringify(current_list)) */
                     } else {
                         console.log("this items amount needs to be updated in the current list");
                         current_list[list_item].amount = new_list[index].amount;
                     }    
                 }
-                //current_list = holder_list;
                 current_list = placeholder_list;
                 component.shopping_list = current_list;
-                console.log("component shopping list:");
-                console.log(component.shopping_list);
                 localStorage.setItem('shopping_list', JSON.stringify(component.shopping_list))
-                console.log("local storage shopping list");
-                console.log(localStorage.shopping_list)
 
             } else if (current_list_length < new_list_length){
                  // if the current list is shorter than the new list, need to add some items before updating the list
                 console.log("the current list is shorter, so we'll need to add some items!");
                 for (let item in new_list){
-                    //console.log(new_list[item].item);
                     let index = current_list.findIndex(x => x.item == new_list[item].item);
-                    //console.log(index);
                     if (index === -1){
                         console.log("this item is in the new list but not the current list - needs to be added!");
                         let list_item = {
@@ -334,8 +205,6 @@ export default {
                                 checked: false,
                                 removed: false
                             }
-                        /* console.log("list item to be added!");
-                        console.log(list_item); */
                         current_list.push(list_item)
                     }
                     if (index != -1){
@@ -344,11 +213,7 @@ export default {
                     }
                 }
                 component.shopping_list = current_list;
-/*                 console.log("component shopping list:");
-                console.log(component.shopping_list); */
                 localStorage.setItem('shopping_list', JSON.stringify(component.shopping_list))
-                console.log("local storage shopping list");
-                console.log(localStorage.shopping_list)
 
             } else if (current_list_length === new_list_length){
                 // if the current list is the same length as the new list, we can assume no items have changed, so just need to update any amounts
@@ -358,28 +223,8 @@ export default {
                     current_list[item].amount = new_list[index].amount;
                 }
                 component.shopping_list = current_list;
-                console.log("component shopping list:");
-                console.log(component.shopping_list);
                 localStorage.setItem('shopping_list', JSON.stringify(component.shopping_list))
-                console.log("local storage shopping list");
-                console.log(localStorage.shopping_list)
             }
-
-
-
-        /*     //check if new_list and current_list match
-            var x = current_list.map(function(item, index) {
-                console.log("in them map function");
-                console.log(item)
-                console.log(new_list[index])
-            // In this case item correspond to currentValue of array a, 
-            // using index to get value from array b
-            //return item - new_list[index];
-            })
-            console.log(x);
- */
-
-
 
         });
 
